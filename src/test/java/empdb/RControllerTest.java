360e4b6b19b5735039346d7983ad64b88208e479
unknown <rcakshat@gmail.com>
2021-01-29 11:39:31 +0530

    Added 2 test cases for error handling

--- src/test/java/empdb/RControllerTest.java
+++ src/test/java/empdb/RControllerTest.java
@@ -57,6 +57,31 @@ public class RControllerTest extends AbstractTest {
     }
 
     @Test
+    public void deleteTest2() throws Exception {
+        String uri = "/delete/2";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertTrue(content.contains("Could not find employee"));
+    }
+
+    @Test
+    public void updateTest2() throws Exception {
+        String uri = "/update/6";
+        Emp emp1=new Emp(6,"BB",27,5,"Python","9009999990");
+        String inputJson = super.mapToJson(emp1);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertTrue(content.contains("Could not find employee"));
+    }
+
+    @Test
     public void updateTest() throws Exception {
         String uri = "/update/3";
         Emp emp1=new Emp(3,"BB",27,5,"Python","9009999990");
